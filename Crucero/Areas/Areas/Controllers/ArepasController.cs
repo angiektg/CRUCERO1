45c99cf93a4143a0e7a97edcbdfe1475955a04b5
Angie Katherine Torres Galindo <antorres@unal.edu.co>
2019-05-29 18:50:23 -0500

    Agregar archivos de proyecto.

---
+++ Crucero/Areas/Areas/Controllers/ArepasController.cs
@@ -0,0 +1,123 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using BDContext;
+
+namespace Crucero.Areas.Areas.Controllers
+{
+    public class ArepasController : Controller
+    {
+        private cruceroEntities db = new cruceroEntities();
+
+        // GET: Areas/Arepas
+        public ActionResult Index()
+        {
+            return View(db.restaurante.ToList());
+        }
+
+        // GET: Areas/Arepas/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            restaurante restaurante = db.restaurante.Find(id);
+            if (restaurante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurante);
+        }
+
+        // GET: Areas/Arepas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Areas/Arepas/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,nombre,horario,principal,capacidad")] restaurante restaurante)
+        {
+            if (ModelState.IsValid)
+            {
+                db.restaurante.Add(restaurante);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(restaurante);
+        }
+
+        // GET: Areas/Arepas/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            restaurante restaurante = db.restaurante.Find(id);
+            if (restaurante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurante);
+        }
+
+        // POST: Areas/Arepas/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,nombre,horario,principal,capacidad")] restaurante restaurante)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(restaurante).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurante);
+        }
+
+        // GET: Areas/Arepas/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            restaurante restaurante = db.restaurante.Find(id);
+            if (restaurante == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurante);
+        }
+
+        // POST: Areas/Arepas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            restaurante restaurante = db.restaurante.Find(id);
+            db.restaurante.Remove(restaurante);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Crucero/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Crucero.MvcApplication" Language="C#" %>
